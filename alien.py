ecb818c345960ace95c61fa178e778a1053f7f7d
Ayo-blvck <martinsayo97@gmail.com>
2024-05-31 02:25:30 +0100

    added rows of aliens, aliens move to end of screem

--- alien.py
+++ alien.py
@@ -5,10 +5,11 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     """A class to represent a single alien in the fleet"""
     
-    def __init__ (self, ai_game):
+    def __init__(self, ai_game):
         """Initialize the slien and set its starting position"""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         
         #Load alien and set its rect attribute
         self.image = pygame.image.load('images/alien.bmp')
@@ -20,3 +21,8 @@ class Alien(Sprite):
         
         #Store the alien's exact horizontal position
         self.x = float(self.rect.x)
+    
+    def update(self):
+        """Move the aliens to the right"""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
