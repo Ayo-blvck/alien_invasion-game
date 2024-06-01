aee8f14951f2e3489af1e5675d44d39cfd628293
Ayo-blvck <martinsayo97@gmail.com>
2024-06-01 23:53:09 +0100

    code refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,12 +36,11 @@ class AlienInvasion:
             self.ship.update()
             self.bullets.update()
             self._update_bullets()
-            # self._update_aliens()
+            self._update_aliens()
             self._update_screen()
             
             
-
-        # Watch for keyboard and mouse events.
+    # Watch for keyboard and mouse events.
     def _check_events(self):
         """Responds to keypresses and mouse events"""
         for event in pygame.event.get():
@@ -54,7 +53,8 @@ class AlienInvasion:
                     
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-                    
+
+
     def _check_keydown_events(self, event):
         """Responds to key presses"""
         if event.key == pygame.K_RIGHT:
@@ -65,68 +65,79 @@ class AlienInvasion:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
-    
+
+
     def _check_keyup_events(self, event):
         """Responds to key releases"""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
-            
+
+
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullet group"""
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
-    
+
+
     def _update_bullets(self):
         #Remove bullets that have disappeared (aviod consuming memory)
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+
     def _create_fleet(self):
         """Create the fleet of aliens"""
         #Make an alien and find number of aliens in a row
-        #Spacing between each alien is eqaul to one alien width
+        #Spacing between each alien is equal to one alien width
         alien = Alien(self)
-        # alien_width, alien_height = int(alien.rect.width // 1.8)
         alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
-        number_alien_x = available_space_x // (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
         
-        # Determine the number of rows of alins that fit on the screen
+        # Alien to Screen Calculation Test 2
+        # alien_width, alien_height = int(alien.rect.size // 1.8)
+        # available_space_x = self.settings.screen_width - (alien_width + (alien_width / 2))
+        # number_aliens_x = int(available_space_x // (alien_width + (alien_width / 2)))
+        
+        # Determine the number of rows of aliens that fit on the screen
         ship_height = self.ship.rect.height
         available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
         number_rows = available_space_y // (2 * alien_height)
         
         # Create the first row of aliens
         for row_number in range(number_rows):
-            for alien_number in range(number_alien_x):
+            for alien_number in range(number_aliens_x):
                 self._create_alien(alien_number, row_number)
-        
+
+
     def _create_alien(self, alien_number, row_number):
         """Create an alien and place it in a row"""
-            #Create an alien and place it in the row
+        #Create an alien and place it in the row
         alien = Alien(self)
         alien_width, alien_height = alien.rect.size
         alien_x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien_x
-        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        alien.rect.y = alien_height + 2 * alien_height * row_number
         self.aliens.add(alien)
-        
+
+
     def _update_aliens(self):
         """Update the positions of all aliens in the fleets"""
         self.aliens.update()
-        
-            #Redraw the screen during each pass through the loop
+
+
+    #Redraw the screen during each pass through the loop
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-        self.aliens.draw(self.screen)
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         
         # Make the most recently drawn screen visible.
         pygame.display.flip()
